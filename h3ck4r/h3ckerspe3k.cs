3f30d53a7e6c209d8fa5c3be97127f875d1e9515
c1opti <c1opti@mail.ru>
2024-01-24 15:18:41 +0300

    word.replace
    h4ck3r sp34k

---
+++ h3ck4r/h3ckerspe3k.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ha3ck4r_sp3ik
+{
+    class h3ckerspe3k
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(Algo(word));
+        }
+
+        static string Algo(string word)
+        {
+            word = word.Replace('a', '4');
+            word = word.Replace('e', '3');
+            word = word.Replace('i', '1');
+            word = word.Replace('s', '5');
+            return word;
+        }
+    }
+}
