a268dafc6dc8a7864dd162d681d19b3eeb6377d5
c1opti <c1opti@mail.ru>
2024-01-24 14:57:23 +0300

    gypotezakollatza
    ezshit

---
+++ gypoteza/gypoteza.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(Collatz(n));
+    }
+
+    static int Collatz(int n)
+    {
+        int answer = n;
+        int counter = 0;
+        while (answer !=1)
+        {
+            if (answer % 2 == 0)
+            {
+                answer = answer / 2;
+            }
+            else
+            {
+                answer = answer * 3 + 1;
+            }
+            counter++;
+        }
+        return counter;
+    }
+}
