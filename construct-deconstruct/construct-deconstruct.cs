8b1e8732901101f4d41e2603f0f08a0345eb10d8
c1opti <c1opti@mail.ru>
2024-01-30 22:32:46 +0300

    some de/construct boring training
    my brain is exploded lol 30.01

---
+++ construct-deconstruct/construct-deconstruct.cs
@@ -0,0 +1,39 @@
+﻿namespace constructor_deconstruct
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Alice", 20, new int[] { 90, 80, 70 });
+            s.Print();
+            
+        }
+    }
+    public class Student
+    {
+        public string name;
+        public int age;
+        public int[] grades;
+
+        public Student(string name, int age, int[] grades)
+        {
+            this.name = name;
+            this.age = age;
+            this.grades = grades;
+        }
+        public void Deconstruct(out string dename, out int deage, out int[] degrades)
+        {
+            dename = name + " JUSTIN";
+            deage = age + 10;
+            degrades = grades;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}, Grades: {string.Join(", ", grades)}");
+            (string dename, int deage, int[] degrades) = this;
+            Console.WriteLine($"Name: {dename}, Age: {deage}, Grades: {string.Join(", ", degrades)}");
+
+        }
+        
+    }
+}
