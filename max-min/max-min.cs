3047190d493bade49291d325505ee1e2720af3b6
c1opti <c1opti@mail.ru>
2024-02-05 22:46:05 +0300

    maxmin task
    arrayconvert/for-cycles

---
+++ max-min/max-min.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class maxmin
+{
+    public static string HighAndLow(string numbers)
+    {
+        int[] numbersarray = Array.ConvertAll(numbers.Split(' '), int.Parse);
+        int indexcount = 0;
+        int indexcount_two = 0;
+        for (int i = 0; i < numbersarray.Length; i++)
+        {
+            if (numbersarray[indexcount] < numbersarray[i])
+            {
+                indexcount = i;
+            }
+        }
+        for (int z = 0; z < numbersarray.Length; z++)
+        {
+            if (numbersarray[indexcount_two] > numbersarray[z])
+            {
+                indexcount_two = z;
+            }
+        }
+        return String.Format("{0} {1}", numbersarray[indexcount], numbersarray[indexcount_two]);
+    }
+}
